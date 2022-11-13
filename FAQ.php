500a831a14fd3c4819b0a748ec73964e6fca998a
Prakash4844 <pk484442@gmail.com>
2022-11-13 16:21:15 +0530

    Added PHP Partials Which Significantly Reduced Code, Almost 70% Code is Reduced.

---
+++ FAQ.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dressingnity FAQ</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- Bootstrap JS Bundle
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        </script> -->
+    <!-- RemixIcon CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- Google Fonts CDN -->
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- Styles.css -->
+    <link rel="stylesheet" href="CSS/Styles.css">
+    <link rel="stylesheet" href="CSS/FAQ.css">
+
+</head>
+
+<body>
+    <!-- Navbar Starts -->
+    <?php
+        include('Partials\Navbar.html')
+    ?>
+    <!-- Navbar Ends -->   
+
+    <section id="faq">
+        <h1 class="faq-heading">FAQ</h1>
+        <div class="container faq">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="FAQ-section">
+
+                            <!-- 1st FAQ menu -->
+                            <div class="FAQ-item">
+                                <div class="FAQ-title">
+                                    <details>
+                                        <summary class="FAQ-descp">
+                                            How do I login on Dressingnity ?
+                                        </summary>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, sunt.
+                                    </details>
+
+                                </div>
+                                <div class="FAQ-content" id="item4">
+
+                                </div>
+                            </div>
+
+                            <!-- 2nd FAQ menu -->
+                            <div class="FAQ-item">
+                                <div class="FAQ-title">
+                                    <details>
+                                        <summary class="FAQ-descp">
+                                            How do I cart the prodcut ?
+                                        </summary>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, sunt.
+                                    </details>
+
+                                </div>
+                            </div>
+
+                            <!-- 3rd FAQ menu -->
+                            <div class="FAQ-item">
+
+
+                                <div class="FAQ-title">
+                                    <details>
+                                        <summary class="FAQ-descp">
+                                            How do I cancel my Order ?
+                                        </summary>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, sunt.
+                                    </details>
+
+                                </div>
+                            </div>
+
+                            <!-- 4th FAQ menu -->
+                            <div class="FAQ-item">
+                                <div class="FAQ-title">
+                                    <details>
+                                        <summary class="FAQ-descp">
+                                            how do I know more about Dressingity ?
+                                        </summary>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis, sunt.
+                                    </details>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- <div class="none"><img src="Images/Wave SVG.svg" alt=""></div> -->
+
+    
+    <!-- Footer Starts -->
+    <?php
+        include('Partials\Footer.html')
+    ?>
+    <!--Footer Ends-->
+
+    <!-- Cookie Banner -->
+    <?php
+    include('Partials\Cookies.html')
+    ?>
+    <!-- End of Cookie Banner -->
+
+    <!-- Custom JS -->
+    <script src="JavaScript/Cookies.js"></script>
+    <!-- Custom JS -->
+
+</body>
+
+</html>
---
+++ Products.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dressingnity Products</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- RemixIcon CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- Google Fonts CDN -->
+
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- Styles.css -->
+    <link rel="stylesheet" href="CSS/Styles.css">
+
+</head>
+
+<body>
+    <!-- Navbar Starts -->
+    <?php
+        include('Partials\Navbar.html')
+    ?>
+    <!-- Navbar Ends -->
+
+    <div class="none"><img src="Images/Wave SVG.svg" alt=""></div>
+
+
+    <!-- Footer Starts -->
+    <?php
+        include('Partials\Footer.html')
+    ?>
+    <!--Footer Ends-->
+
+    <!-- Cookie Banner -->
+    <?php
+    include('Partials\Cookies.html')
+    ?>
+    <!-- End of Cookie Banner -->
+
+    <!-- Custom JS -->
+    <script src="JavaScript/Cookies.js"></script>
+    <!-- Custom JS -->
+
+</body>
+
+</html>
