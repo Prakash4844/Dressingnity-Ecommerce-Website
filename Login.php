e05ff48804cb27b595acabc57c3624b166278d83
Prakash4844 <pk484442@gmail.com>
2022-11-25 20:44:00 +0530

    Added JS in Main file as to keep only one instance of Bootstrap JS bundle per file.

--- Login.php
+++ Login.php
@@ -86,7 +86,13 @@
     include('Partials/Cookies.php')
     ?>
     <!-- End of Cookie Banner -->
-
+    
+    <!-- Bootstrap JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+        crossorigin="anonymous"></script>
+    <!-- Bootstrap JS Bundle -->
+    
     <!-- Custom JS -->
     <script src="JavaScript/Cookies.js"></script>
     <!-- Custom JS -->
