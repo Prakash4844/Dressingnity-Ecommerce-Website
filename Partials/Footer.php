7cd615c632b939ade281eaf3f7105573fae96061
Prakash4844 <pk484442@gmail.com>
2022-11-30 11:33:13 +0530

    Removed Conflicting HEAD code in Partials

--- Partials/Footer.php
+++ Partials/Footer.php
@@ -1,16 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dressingnity Home page</title>
-
-</head>
-
 <body>
-
         <!-- Footer Starts -->
         <footer class="Dressingnity_footer">
             <div class="widget_wrapper">
