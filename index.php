da3f0420e5d065922ba8e017d224ce6b3a3c1036
Prakash4844 <pk484442@gmail.com>
2022-12-02 10:42:07 +0530

    Removed Login alert from Homepage

--- index.php
+++ index.php
@@ -39,23 +39,6 @@
         include('Partials/Navbar.php')
     ?>
 
-
-<?php
-// PHP program to pop an alert
-// message box on the screen
-  
-// Function definition
-if(ISSET($_SESSION['success'])){
-function_alert("Sucessfully Logged in");
-}
-  
-function function_alert($message) {
-    // Display the alert box 
-    echo "<script>alert('$message');</script>";
-} 
-// Function call
-  
-?>
     <!-- Navbar Ends -->
 
 
