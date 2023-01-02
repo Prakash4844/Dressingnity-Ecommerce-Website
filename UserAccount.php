80abfc1407ec854d1d3c8bbb63501606dd6501b6
Prakash4844 <pk484442@gmail.com>
2023-01-02 19:08:16 +0530

    Formatted code added missing fevicon

--- UserAccount.php
+++ UserAccount.php
@@ -5,27 +5,33 @@ session_start();
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Account</title>
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account</title>
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <!-- RemixIcon CDN -->
-  <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- RemixIcon CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
 
-  <!-- Font Awesome CDN -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+      integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
-  <!-- DarkReader JavaScript CDN -->
-  <script src="https://cdn.jsdelivr.net/npm/darkreader@4.9.58/darkreader.min.js"></script>
+    <!-- DarkReader JavaScript CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/darkreader@4.9.58/darkreader.min.js"></script>
 
-  <link rel="stylesheet" href="CSS/Styles.css">
-  <link rel="stylesheet" href="CSS/Login.css">
-  <link rel="stylesheet" href="CSS/UserAccount.css">
+    <link rel="stylesheet" href="CSS/Styles.css">
+    <link rel="stylesheet" href="CSS/Login.css">
+    <link rel="stylesheet" href="CSS/UserAccount.css">
+
+    <!-- Favicon -->
+    <link rel="icon" href="Images/SVG Samples/Dressingnity recolor Logo.png">
 
 </head>
 
@@ -48,31 +54,49 @@ session_start();
         <div class="card border-0 shadow rounded-3 my-5">
           <div align=center class="card-body p-4 p-sm-5">
             <i class="fa-solid fa-circle-user extra-lg vio-outline"></i>
-            <hr><h6 align=left>Email</h6>
+            <hr>
+            <h6 align=left>Email</h6>
             <form action="Logout.php" class="login-form">
               <div class="form-floating mb-3 input-area Play_font">
                 <input type="text" class="form-control no-outline" id="floatingEmail" placeholder="xyzEmail" disabled>
-                <label for="floatingEmail"><i style="margin-right: 15px;" class="icon fas fa-envelope vio-outline"></i><?php echo $_SESSION['loggedemail'] ?></label>
+                <label for="floatingEmail"><i style="margin-right: 15px;" class="icon fas fa-envelope vio-outline"></i>
+                  <?php echo $_SESSION['loggedemail'] ?>
+                </label>
               </div>
-              <hr class="my-4"><h6 align=left>First Name</h6>
+              <hr class="my-4">
+              <h6 align=left>First Name</h6>
               <div class="form-floating mb-3 input-area Play_font">
                 <input type="number" class="form-control no-outline" id="floatingFirstName" placeholder="John" disabled>
-                <label for="floatingLastname"><i style="margin-right: 15px;" class="icon fa fa-user-plus vio-outline" aria-hidden="true"></i><?php echo $_SESSION['loggedfirstname'] ?></label>
+                <label for="floatingLastname"><i style="margin-right: 15px;" class="icon fa fa-user-plus vio-outline"
+                    aria-hidden="true"></i>
+                  <?php echo $_SESSION['loggedfirstname'] ?>
+                </label>
               </div>
-              <hr class="my-4"><h6 align=left>Last Name</h6>
+              <hr class="my-4">
+              <h6 align=left>Last Name</h6>
               <div class="form-floating mb-3 input-area Play_font">
                 <input type="text" class="form-control no-outline" id="floatingLastName" placeholder="Doe" disabled>
-                <label for="floatingFirst"><i style="margin-right: 15px;" class="icon fa fa fa-user-plus vio-outline" aria-hidden="true"></i><?php echo $_SESSION['loggedlastname'] ?></label>
+                <label for="floatingFirst"><i style="margin-right: 15px;" class="icon fa fa fa-user-plus vio-outline"
+                    aria-hidden="true"></i>
+                  <?php echo $_SESSION['loggedlastname'] ?>
+                </label>
               </div>
-              <hr class="my-4"><h6 align=left>Username</h6>
+              <hr class="my-4">
+              <h6 align=left>Username</h6>
               <div class="form-floating mb-3 input-area Play_font">
                 <input type="email" class="form-control no-outline" id="floatingInput" placeholder="Username" disabled>
-                <label for="floatingUsername"><i style="margin-right: 15px;" class="icon fa-solid fa-user vio-outline"></i><?php echo $_SESSION['loggedusername'] ?></label>
+                <label for="floatingUsername"><i style="margin-right: 15px;"
+                    class="icon fa-solid fa-user vio-outline"></i>
+                  <?php echo $_SESSION['loggedusername'] ?>
+                </label>
               </div>
-              <hr class="my-4"><h6 align=left >Password</h6>
+              <hr class="my-4">
+              <h6 align=left>Password</h6>
               <div class="form-floating mb-3 input-area Play_font">
-                <input type="password" class="form-control no-outline" id="floatingPassword" placeholder="********" disabled>
-                <label for="floatingPassword"><i style="margin-right: 15px;" class="icon fas fa-lock vio-outline"></i>********</label>
+                <input type="password" class="form-control no-outline" id="floatingPassword" placeholder="********"
+                  disabled>
+                <label for="floatingPassword"><i style="margin-right: 15px;"
+                    class="icon fas fa-lock vio-outline"></i>********</label>
               </div>
               <input type="submit" name="login" value="Logout">
 
@@ -99,7 +123,9 @@ session_start();
   <!-- End of Cookie Banner -->
 
   <!-- Bootstrap JS Bundle -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+    crossorigin="anonymous"></script>
   <!-- Bootstrap JS Bundle -->
 
   <!-- Custom JS -->
